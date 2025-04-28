1e43f6fa176b74cb857679b8a9ce92a84dece078
Leonardo Lastra <leonardo.lastra@gmail.com>
2025-04-28 18:12:26 -0400

    limpiar antiguos

--- frontend/src/route/__test__/Form.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Form from '../AgregarPalabras/Form';
-
-test('renders learn react link', () => {
-  render(<Form />);
-  const linkElement = screen.getByText(/Significado/i);
-  expect(linkElement).toBeInTheDocument();
-});
